d2bb2b14a0576bb68d66293f191d036b5ef11542
ginos1998 <ginosiam1998@gmail.com>
2022-06-03 04:08:50 -0300

    V1.0.1 | Se agrego: pantalla principal, menu config, menu seleccion (+6 personajes)

--- CodeMonkeys/src/main/java/com/example/codemonkeys/HelloApplication.java
+++ CodeMonkeys/src/main/java/com/example/codemonkeys/HelloApplication.java
@@ -2,22 +2,32 @@ package com.example.codemonkeys;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class HelloApplication extends Application {
-    @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
-        stage.setScene(scene);
-        stage.show();
-    }
+
 
     public static void main(String[] args) {
-        launch();
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+
+        Parent root = FXMLLoader.load(getClass().getResource("hello-view.fxml"));
+        Scene scene = new Scene(root, Color.WHITE);
+
+        stage.setScene(scene);
+        stage.setTitle("Fakemon");
+        stage.show(); // Muestra la ventana
     }
 }
